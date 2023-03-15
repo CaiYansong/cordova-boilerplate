d0ccac6fc5c65170cfc50837fb2918790a666841
CaiYansong <caiyansong163@163com>
2023-03-15 10:00:38 +0800

    fix: build-release

--- build-release.js
+++ build-release.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs-extra");
 const { exec, execSync } = require("child_process");
 
 const releaseInfo = require("./.release-info.js");
@@ -26,6 +27,7 @@ module.exports = {
 */
 
 function buildRelease() {
+  handleXML("BUILD", "/");
   execSync("npm run dev-build", { stdio: "inherit" });
   cleanUp();
   const buildCmd = `cordova run android --release --prod -- --keystore=${releaseInfo.storeName} --storePassword=${releaseInfo.storePassword} --alias=${releaseInfo.alias} --password=${releaseInfo.password} --packageType=${releaseInfo.packageType}`;
@@ -44,6 +46,37 @@ function buildRelease() {
     },
   );
   build.stdout.pipe(process.stdout);
+}
+
+/**
+ * 处理 XML，在 Server 模式下指向 devServer 的链接
+ * @param {string} mode
+ * @param {string} url
+ */
+function handleXML(mode, url = "/") {
+  const cordovaConfigPath = path.resolve(__dirname, "./cordova/config.xml");
+
+  let cordovaConfig = fs.readFileSync(cordovaConfigPath, "utf-8");
+  const lines = cordovaConfig.split(/\r?\n/g).reverse();
+
+  const regexContent = /\s+<content/;
+  const contentIndex = lines.findIndex((line) => line.match(regexContent));
+  if (contentIndex >= 0) {
+    lines[contentIndex] = `  <content src="${url}" />`;
+  }
+
+  const iosDevComment = /\s+<!-- iOS 调试模式允许 Http -->/;
+  const iosDevIndex = lines.findIndex((line) => line.match(iosDevComment));
+  if (iosDevIndex >= 0) {
+    if (mode === "SERVE") {
+      lines[iosDevIndex - 1] = `  <allow-navigation href="${url}" />`;
+    } else {
+      lines[iosDevIndex - 1] = `  <!-- <allow-navigation href="${url}" /> -->`;
+    }
+  }
+
+  cordovaConfig = lines.reverse().join("\n");
+  fs.writeFileSync(cordovaConfigPath, cordovaConfig);
 }
 
 function cleanUp(platform) {
